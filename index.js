eac0466658d7f311bade6bdf927c59d41b1ed174
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-23 15:24:27 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, stringName) {
+  return drivers.filter(name =>
+    name.toLowerCase() === stringName.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, partialStr) {
+  return drivers.filter(name =>
+    // returns the first index at which a given element can be found in the array, else -1
+    name.toLowerCase().indexOf(partialStr.toLowerCase()) === 0
+  )
+}
+
+
+function matchName(driversObjs, stringName) {
+  return driversObjs.filter(i => i.name === stringName)
+}
